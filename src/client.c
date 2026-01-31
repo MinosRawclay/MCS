a8dce02c363e9df57322bb0c167c0cc77fdde15d
Maxime N. <maxime.neuquelman@gmail.com>
2026-01-31 16:05:26 +0100

    Suite du développement de la partie client

--- src/client.c
+++ src/client.c
@@ -1,4 +1,7 @@
 
+// TODO : Documentation
+// TODO : Mode DEBUG ?
+
 #include <stdio.h>
 #include "../include/data.h"
 #include "../include/libDial.h"
@@ -6,14 +9,52 @@
 #include "../include/session.h"
 
 
+// DANS MOTEUR.H :
+
+// Pas terminée
+void showCard(enum card){
+
+    const char colorSymbols[4] = {'♥', '⯁', '♠', '♣'};
+
+    printf("%s\n", colorSymbols[card / 8]);
+
+}
+
+
+/**
+ *	\fn			void showPlayerCards(const player_t *player)
+ *	\brief		Affiche les cartes d'un joueur
+ *	\note		
+ *	\result		
+ */
+void showPlayerCards(const player_t *player){
+
+    if (player == NULL){
+        return;
+    }
+
+    for (char i = 0; i < nbCards / 2 - 1; i++){
+        printf("%d : %s\t\t|\t\t%d : %s\n", i, showCard(player->cards[i]), i + 1, showCard(player->cards[i]));
+    }
+
+    // Nombre de cartes impaire :
+    if (nbCards % 2 == 1){
+        printf("%d : %s\t\t|\n", nbCards - 1, showCard(player->cards[nbCards - 1]));
+    }
+}
+
+// FIN
+
+
 /**
  *	\fn			void threadClient(void)
  *	\brief		Traitement du client
- *	\note		si le paramètre deSerial vaut NULL alors quoi est une chaîne de caractères
- *	\result		paramètre quoi modifié avec le requête/réponse reçue
- *				paramètre sockEch modifié pour le mode DGRAM
+ *	\note		
+ *	\result		
  */
 void threadClient(void){
+
+    player_t player;
 
     // Socket d'appel :
     socket_t saServer;
@@ -23,7 +64,7 @@ void threadClient(void){
     
 
     // Connexion avec le serveur :
-    saServer = creerSocketAdr(adresseServeur, portServeur);
+    saServer = creerSocketAdr(serverAddr, serverPort);
 
     printf("Vous avez rejoint la partie de ...\n");
 
@@ -37,28 +78,28 @@ void threadClient(void){
 
     // Début d'une manche :
 
-    traiterRequetesPartie(&saServer, &request);
+    gameRequestHandler(&saServer, &request);
 
     return;
 }
 
 
-void waitForRequest(socket_t *sockEch, requete_t *request, requeteList_t requestNb){
+void waitForRequest(socket_t *sockEch, requete_t *request, requeteList_t requestCode){
 
     request->idReq = -1;
 
-    while (requete.idReq != REQ_PARTIE_LANCEE){
+    while (requete.idReq != requestCode){
         recevoir(sockEch, request, str2req);
 
-        if (requete.idReq != REQ_PARTIE_LANCEE){
+        if (requete.idReq != requestCode){
 
             // Réponse PAS OK à faire
 
-            envoyer(sockEch, response, str2req);
+            envoyer(sockEch, response, rep2str);
         } else {
 
             // Réponse OK à faire
-            envoyer(sockEch, response, str2req);
+            envoyer(sockEch, response, rep2str);
 
         }
     }
@@ -68,32 +109,147 @@ void waitForRequest(socket_t *sockEch, requete_t *request, requeteList_t request
 
 
 
-void traiterRequetesPartie(socket_t *sockEch, requete_t *request){
+void gameRequestHandler(socket_t *sockEch, requete_t *request, player_t *currentPlayer){
 
     while (1){
 
         recevoir(sockEch, request, str2req);
 
-        switch (request->idReq)
-        {
-        case constant expression:
+        switch (request->idReq){
+
+            case REQ_ENVOI_DECK:
+                getCards(sockEch, currentPlayer);
+                break;
+
+            case REQ_ENVOI_PLI:
+                getPli();
+                break;
+
+            case REQ_ENVOI_ATOUT:
+                keepTrump();
+                break;
+
+            case REQ_FIN_PARTIE:
+                // + message, option pour relancer etc... ?
+                afficherScore();
+
+                break;
             
-            break;
-        
-        default:
-            break;
+            default:
+                perror("Requête reçu non prévue par le client...");
+                break;
         }
 
     }
 
 }
 
-void recupererCartes(){
+void getCards(socket_t *sockEch, player_t *currentPlayer){
+
+    requete_t *request;
+    reponse_t *response;
+
+    player_t *player;
+
+    recevoir(sockEch, request, str2req);
+
+    response->idRep = REQ_OK;
+    strcpy(response->verbRep, "OK");
+
+    envoyer(sockEch, response, rep2str);
+
+    // Ajout des cartes au deck :
+    memcpy(currentPlayer->cards + len(currentPlayer->cards), player->cards, sizeof(len(player->cards)));
+
     
 }
 
-void recupererPli(){
+void getPli(socket_t *sockEch, player_t *currentPlayer){
+
+    requete_t *request;
+    reponse_t *response;
+
+    pli_t pli;
+
+
+    recevoir(sockEch, request, str2req);
+
+    // Affichage du pli :
+    printf("Le pli est : ...\n");
+
+    envoyer(sockEch, OK, ...);
+
+    return;
 
 }
 
+
+void keepTrump(){
+
+    requete_t *request;
+    reponse_t *response;
+
+    pli_t pli;
+
+    char userResponse;
+
+    recevoir(sockEch, request, str2req);
+
+    // Affichage du pli :
+    printf("L'atout est : ...\n");
+
+    // Demande pour savoir si l'utilisateur veut prendre le pli :
+    printf("Prendre l'atout (y-N) ? ");
+    
+    userResponse = getchar();
+
+    // fgets(userResponse, 1 * sizeof(char), stdin);
+    fflush(stdin);
+
+    // Prise du pli :
+    if (tolower(userResponse) == 'y'){
+        response->idRep = REQ_TAKE_PLI;
+        strcpy(response->verbRep, "take");
+
+        currentPlayer->cards[...] = pli;
+
+    // Non prise du pli :
+    } else {
+        response->idRep = REQ_LET_PLI;
+        strcpy(response->verbRep, "let");
+    }
+
+    // Envoi de la réponse au serveur :
+    envoyer(sockEch, response, rep2str);
+}
+
+
+void playMove(socket_t *sockEch, player_t *currentPlayer){
+
+    enum card selectedCard = -1;
+
+    reponse_t response;
+
+    printf("C'est à vous de jouer !\n\n");
+
+    printf("Affichage de vos cartes :\n");
+    showCards(currentPlayer);
+
+    while (selectedCard < 0 || nbCards - 1 < selectedCard){
+        
+        printf("Entrez l'indice de la carte à jouer (entre 0 et %d) : ", nbCards - 1);
+        
+        selectedCard = atoi(getchar());
+        fflush(stdin);
+    }
+
+    // Création de la réponse pour le serveur :
+    response.idRep = REP_PLAY;
+    strcpy(response.verbRep, "PLAY");
+    response.optRep = selectedCard;
+
+    // Envoi de la réponse au serveur :
+    envoyer(sockEch, response, ...);
+
+}
 // ...
